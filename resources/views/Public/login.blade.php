a4e83c7d18c90e5450e94b315881a1bb766aaece
Kagayaku Kagami <jianyao2555@gmail.com>
2024-06-30 19:34:35 +0700

    Update View Login & Register

--- resources/views/Public/login.blade.php
+++ resources/views/Public/login.blade.php
@@ -5,27 +5,57 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>Login Bioskop</title>
+    <title>Document</title>
 
     <link rel="stylesheet" href="/css/styles.css">
+    <link href="{{ asset('css/flowbite.css') }}" rel="stylesheet">
 </head>
-<body class="bg-teal-300 flex items-center justify-center min-h-screen">
-    <form class="max-w-sm w-full bg-white p-6 rounded-lg shadow-md">
+
+<body class="flex items-center justify-center min-h-screen 
+             font-roboto pt-16 bg-neutral-900">
+    {{-- Navbar --}}
+    <nav class="bg-emerald-800 text-gray-200 z-20 h-16
+                border-emerald-800 border-b-2 top-0 fixed w-full">
+        <div class="max-w-screen mx-auto flex justify-between">
+            <a href="/" class="flex flex-row items-center gap-2 p-4">
+                <div class="font-medium text-xl">Web Bioskop</div>
+            </a>
+            <div class="w-auto mx-3 flex flex-row gap-1 self-center">
+                <a class="py-2 px-3 flex flex-row items-center rounded-md shadow-xl
+                          cursor-pointer hover:bg-emerald-700 active:bg-emerald-900/50"
+                          href="/login">
+                    <i class="bi bi-box-arrow-in-right mr-2 text-2xl"></i>
+                    <div class="text-lg px-2"> Login </div>
+                </a>
+            </div>
+        </div>
+    </nav>
+    
+    <form class="p-6 max-w-sm w-full flex flex-col mx-auto bg-neutral-800/80 
+                  rounded-lg shadow-lg border border-neutral-700">
+        <div class="mb-3 text-2xl font-bold text-white">Login Bioskop</div>
         <div class="mb-5">
-          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-          <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+            <label class="block mb-2 text-sm font-medium text-gray-200" for="email">Input email</label>
+            <input type="email" placeholder="YourEmail@example.com" required
+                   class="block w-full p-2.5 border bg-neutral-700 border-neutral-800 text-gray-200 
+                          text-sm rounded-lg focus:ring-emerald-700 focus:border-emerald-700"/>
         </div>
-        <div class="mb-5">
-          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-          <input type="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+        <div class="mb-8">
+            <label for="password" class="block mb-2 text-sm font-medium text-gray-200">Input password</label>
+            <input type="password" id="password" required 
+                   class="block w-full p-2.5 border bg-neutral-700 border-neutral-800 text-gray-200 
+                          text-sm rounded-lg focus:ring-emerald-700 focus:border-emerald-700"/>
         </div>
-        <div class="flex items-start mb-5">
-          <div class="flex items-center h-5">
-            <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
-          </div>
-          <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-        </div>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-      </form>
+        <button class="mb-3 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 
+                       text-center text-white bg-emerald-700 hover:bg-emerald-800 
+                       focus:ring-4 focus:outline-none focus:ring-emerald-300" 
+                       type="submit">Login</button>
+        <p class="text-sm font-light text-gray-300">
+            Don’t have an account yet? 
+            <a href="/register" class="font-medium hover:underline">Register</a>
+        </p>
+    </form>
+
+    <script defer src="{{ asset('js/flowbite.js') }}"></script>
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,5 +17,6 @@ module.exports = {
   plugins: [
     require('flowbite/plugin'),
   ],
+  important: true,
 }
 
