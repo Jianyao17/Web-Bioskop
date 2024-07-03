8d000e7ad2ac662578c9447bf77b9c6594381bd8
DimasBandeng <rtzcreator@gmail.com>
2024-07-03 21:33:11 +0700

    mengupdate controller

--- app/Http/Controllers/Admin/ListFilmController.php
+++ app/Http/Controllers/Admin/ListFilmController.php
@@ -8,6 +8,9 @@ use Livewire\Component;
 // Super Admin & Admin Bioskop
 class ListFilmController extends Component
 {
+    public $poster_film,$judul_film,$durasi,$deskripsi,$status;
+    public $films,$film_id;
+
     public function mount()
     {
         
@@ -17,5 +20,116 @@ class ListFilmController extends Component
     {
         return view('Admin.list-film')
             ->layoutData(['page' => 'Film']);
+    }
+
+    public function store(){
+        $this->validate([
+            'poster_film'      => 'required|string',
+            'judul_film'     => 'required|string',
+            'durasi'  => 'required|string|integer',
+            'deskripsi'      => 'required|string',
+            'status'    =>  'required|string'
+        ]);
+
+
+        Film ::create([
+            'poster_film'  => $this->poster_film,
+            'judul_film'  => $this->judul_film,
+            'durasi'  => $this->durasi,
+            'deskripsi'   => $this->role,
+            'status' => $this->status
+        ]);
+
+        $this->resetValue();
+        $this->dispatchBrowserEvent('close-modal');
+        $this->dispatchBrowserEvent('notify', 
+        [ 'type' => 'success', 'message' => 'Film Berhasil Ditambahkan']);
+
+    }
+
+    public function editFilm($id) 
+    {
+        $film = Film::find($id);
+
+        if (!$film) 
+        {
+            $this->dispatchBrowserEvent('close-modal');
+            $this->dispatchBrowserEvent('notify', 
+            [ 'type' => 'failed', 'message' => 'Film Tidak Ditemukan']);  
+            return;
+        }
+        $this->film_id = $id;
+        $this->poster_film = $film->poster_film; 
+        $this->judul_film = $film->judul_film;
+        $this->durasi = $film->durasi;
+        $this->deskripsi = $film->deskripsi;
+        $this->status = $film->status;
+
+    }
+
+    public function update() 
+    {
+        // Validate data to be updated
+        $this->validate([ 
+            'poster_film'      => 'required|string',
+            'judul_film'     => 'required|string',
+            'durasi'  => 'required|string|integer',
+            'deskripsi'      => 'required|string',
+            'status'    =>  'required|string'
+        ]);
+        
+        // Update data in database
+        Film::find($this->film_id)->update(
+        [
+            'poster_film'  => $this->poster_film,
+            'judul_film'  => $this->judul_film,
+            'durasi'  => $this->durasi,
+            'deskripsi'   => $this->role,
+            'status' => $this->status
+        ]);
+
+        $this->resetValue();
+        $this->dispatchBrowserEvent('close-modal');
+        $this->dispatchBrowserEvent('notify', 
+        [ 'type' => 'success', 'message' => 'Film Berhasil Diperbarui']);
+    }
+
+    public function deleteFilm($id)
+    {
+        $film = Film::find($id);
+
+        if (!$film) 
+        {
+            $this->dispatchBrowserEvent('close-modal');
+            $this->dispatchBrowserEvent('notify', 
+            [ 'type' => 'failed', 'message' => 'Film Tidak Ditemukan']);  
+            return;
+        }
+
+        $this->film_id = $id;
+        $this->poster_film = $film->poster_film; 
+        $this->judul_film = $film->judul_film;
+        $this->durasi = $film->durasi;
+        $this->deskripsi = $film->deskripsi;
+        $this->status = $film->status;
+    }
+
+    public function delete()
+    {
+        Film::find($this->film_id)->delete();
+
+        $this->resetValue();
+        $this->dispatchBrowserEvent('close-modal');
+        $this->dispatchBrowserEvent('notify', 
+        [ 'type' => 'success', 'message' => 'Film Berhasil Dihapus']);
+    }
+
+    public function resetValue()
+    {
+        $this->poster_film = '';
+        $this->judul_film = '';
+        $this->durasi = '';
+        $this->deskripsi = '';
+        $this->status = '';
     }
 }
--- app/Http/Controllers/Admin/PenayanganController.php
+++ app/Http/Controllers/Admin/PenayanganController.php
@@ -7,6 +7,9 @@ use Livewire\Component;
 
 class PenayanganController extends Component
 {
+    public $waktu_tayang,$id_film,$id_bioskop,$id_teater,$harga_tiket,$status;
+    public $penayangan,$penayangan_id;
+
     public function mount()
     {
         
@@ -16,5 +19,125 @@ class PenayanganController extends Component
     {
         return view('Admin.penayangan')
             ->layoutData(['page' => 'Penayangan']);
+    }
+
+    public function store(){
+        $this->validate([
+            'waktu_tayang'      => 'required|string',
+            'id_film'     => 'required|string',
+            'id_bioskop'  => 'required|string|integer',
+            'id_teater'      => 'required|string',
+            'harga_tiket'    =>  'required|string',
+            'status'        => 'required|string'
+
+        ]);
+
+
+        Penayangan ::create([
+            'waktu_tayang'  => $this->waktu_tayang,
+            'id_film'  => $this->id_film,
+            'id_bioskop'  => $this->id_bioskop,
+            'id_teater'   => $this->id_teater,
+            'harga_tiket' => $this->harga_tiket,
+            'status' => $this->status
+        ]);
+
+        $this->resetValue();
+        $this->dispatchBrowserEvent('close-modal');
+        $this->dispatchBrowserEvent('notify', 
+        [ 'type' => 'success', 'message' => 'Penayangan Berhasil Ditambahkan']);
+
+    }
+
+    public function editPenayangan($id) 
+    {
+        $penayangan = Penayangan::find($id);
+
+        if (!$penayangan) 
+        {
+            $this->dispatchBrowserEvent('close-modal');
+            $this->dispatchBrowserEvent('notify', 
+            [ 'type' => 'failed', 'message' => 'Penayangan Tidak Ditemukan']);  
+            return;
+        }
+        $this->penayangan_id = $id;
+        $this->waktu_tayang = $penayangan->waktu_tayang; 
+        $this->id_film = $penayangan->id_film;
+        $this->id_bioskop = $penayangan->id_bioskop;
+        $this->id_teater = $penayangan->id_teater;
+        $this->harga_tiket = $penayangan->harga_tiket;
+        $this->status = $penayangan->status;
+
+    }
+
+    public function update() 
+    {
+        // Validate data to be updated
+        $this->validate([ 
+            'waktu_tayang'      => 'required|string',
+            'id_film'     => 'required|string',
+            'id_bioskop'  => 'required|string|integer',
+            'id_teater'      => 'required|string',
+            'harga_tiket'    =>  'required|string',
+            'status'        => 'required|string'
+        ]);
+        
+        // Update data in database
+        Penayangan::find($this->penayangan_id)->update(
+        [
+            'waktu_tayang'  => $this->waktu_tayang,
+            'id_film'  => $this->id_film,
+            'id_bioskop'  => $this->id_bioskop,
+            'id_teater'   => $this->id_teater,
+            'harga_tiket' => $this->harga_tiket,
+            'status' => $this->status
+        ]);
+
+        $this->resetValue();
+        $this->dispatchBrowserEvent('close-modal');
+        $this->dispatchBrowserEvent('notify', 
+        [ 'type' => 'success', 'message' => 'Penayangan Berhasil Diperbarui']);
+    }
+
+    public function deletePenayangan($id)
+    {
+        $penayangan = Penayangan::find($id);
+
+        if (!$penayangan) 
+        {
+            $this->dispatchBrowserEvent('close-modal');
+            $this->dispatchBrowserEvent('notify', 
+            [ 'type' => 'failed', 'message' => 'Penayangan Tidak Ditemukan']);  
+            return;
+        }
+
+        $this->penayangan_id = $id;
+        $this->waktu_tayang = $penayangan->waktu_tayang; 
+        $this->id_film = $penayangan->id_film;
+        $this->id_bioskop = $penayangan->id_bioskop;
+        $this->id_teater = $penayangan->id_teater;
+        $this->harga_tiket = $penayangan->harga_tiket;
+        $this->status = $penayangan->status;
+
+    }
+
+    public function delete()
+    {
+        Penayangan::find($this->penayangan_id)->delete();
+
+        $this->resetValue();
+        $this->dispatchBrowserEvent('close-modal');
+        $this->dispatchBrowserEvent('notify', 
+        [ 'type' => 'success', 'message' => 'Penayangan Berhasil Dihapus']);
+    }
+
+    public function resetValue()
+    {
+         $this->waktu_tayang = ''; 
+        $this->id_film = '';
+        $this->id_bioskop = '';
+        $this->id_teater = '';
+        $this->harga_tiket = '';
+        $this->status = '';
     }
 }
